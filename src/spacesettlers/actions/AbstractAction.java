6a3f366fd14ad246a346d0c359d65da92ddc86c3
jherrin1 <josiah.herrington@ou.edu>
2018-04-17 14:28:34 -0500

    Add files via upload
    Drone implementation test

--- src/spacesettlers/actions/AbstractAction.java
+++ src/spacesettlers/actions/AbstractAction.java
@@ -9,6 +9,7 @@ import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
+import spacesettlers.objects.Drone;
 import spacesettlers.objects.Ship;
 import spacesettlers.objects.weapons.AbstractWeapon;
 import spacesettlers.simulator.Toroidal2DPhysics;
@@ -35,7 +36,15 @@ abstract public class AbstractAction {
 	 * @return
 	 */
 	abstract public Movement getMovement(Toroidal2DPhysics space, Ship ship);
-
+	
+	/**
+	 * All actions must return a movement in (x,y) and orientation space.
+	 * If the ship is not moving, simply return DoNothingAction.
+	 *   
+	 * @return
+	 */
+	abstract public Movement getMovement(Toroidal2DPhysics space, Drone drone);
+	
 	/**
 	 * Calls the abstract getMovement with supplied timeout for response. If
 	 * nothing is returned in that time, simple return DoNothingAction (new Movement())
@@ -45,6 +54,41 @@ abstract public class AbstractAction {
 	public Movement getMovement(Toroidal2DPhysics space, Ship ship, int timeout){
 		ExecutorService executor = Executors.newSingleThreadExecutor();
         Future<Movement> future = executor.submit(new SpacewarActionCallable(this, space, ship));
+        
+        Movement movement = null;
+        try {
+            //start
+        	movement = future.get(timeout, TimeUnit.MILLISECONDS);
+            //finished in time
+        } catch (TimeoutException e) {
+            //was terminated
+        	//return no movement
+        	movement = new Movement();
+        } catch (InterruptedException e) {
+        	//we were interrupted (should not happen but lets be good programmers) 
+        	//return no movement
+        	movement = new Movement();
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			//the executor threw and exception (should not happen but lets be good programmers) 
+			//return no movement
+        	movement = new Movement();
+		}
+
+        executor.shutdownNow();
+        
+        return movement;
+	}
+	
+	/**
+	 * Calls the abstract getMovement with supplied timeout for response. If
+	 * nothing is returned in that time, simple return DoNothingAction (new Movement())
+	 *   
+	 * @return
+	 */
+	public Movement getMovement(Toroidal2DPhysics space, Drone drone, int timeout){
+		ExecutorService executor = Executors.newSingleThreadExecutor();
+        Future<Movement> future = executor.submit(new SpacewarActionCallable(this, space, drone));
         
         Movement movement = null;
         try {
@@ -87,18 +131,27 @@ abstract public class AbstractAction {
 		private AbstractAction action;
 		private Toroidal2DPhysics space;
 		private Ship ship;
+		private Drone drone;
 		
 		SpacewarActionCallable(AbstractAction action, Toroidal2DPhysics space, Ship ship){
 			this.action = action;
 			this.space = space;
 			this.ship = ship;
 		}
+		SpacewarActionCallable(AbstractAction action, Toroidal2DPhysics space, Drone drone){//herr0861 edit - Overload to handle multiple object types
+			this.action = action;
+			this.space = space;
+			this.drone = drone;
+		}
 		
 		public Movement call() throws Exception {
 			if(this.action != null && this.ship != null && this.space != null){
 				return this.action.getMovement(this.space, this.ship);
-			}else{
+			} else if (this.action != null && this.drone != null && this.space != null) {//herr0861edit
+				return this.action.getMovement(this.space, this.drone);
+			} else {
 				//something went wrong...lets return no movement
+				System.out.println("Error, no movement");
 				return new Movement();
 			}
 			
--- src/spacesettlers/actions/DoNothingAction.java
+++ src/spacesettlers/actions/DoNothingAction.java
@@ -1,5 +1,6 @@
 package spacesettlers.actions;
 
+import spacesettlers.objects.Drone;
 import spacesettlers.objects.Ship;
 import spacesettlers.simulator.Toroidal2DPhysics;
 import spacesettlers.utilities.Movement;
@@ -14,6 +15,15 @@ public class DoNothingAction extends AbstractAction {
 	 * This action never moves the ship
 	 */
 	public Movement getMovement(Toroidal2DPhysics space, Ship ship) {
+		return new Movement();
+	}
+	
+	/**
+	 * This action never moves the drone
+	 * Herr0861 edit
+	 * This is an overloaded method to allow handling of an additional class. In this case, Drone.
+	 */
+	public Movement getMovement(Toroidal2DPhysics space, Drone drone) {
 		return new Movement();
 	}
 
